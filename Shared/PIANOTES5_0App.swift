bb11cb72e2571ca531f98b07c12f15ea0d48a5ad
Chase Smith <chases832@gmail.com>
2021-09-16 13:59:26 -0500

    Initial Commit

---
+++ Shared/PIANOTES5_0App.swift
@@ -0,0 +1,17 @@
+//
+//  PIANOTES5_0App.swift
+//  Shared
+//
+//  Created by Chase Smith on 9/16/21.
+//
+
+import SwiftUI
+
+@main
+struct PIANOTES5_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
